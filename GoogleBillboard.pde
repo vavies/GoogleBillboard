3d9524b692b8d7368a95735259a538826e60e4a6
vavies <olivia38912@gmail.com>
2017-01-05 13:53:58 -0800

    something

--- GoogleBillboard.pde
+++ GoogleBillboard.pde
@@ -1,14 +1,25 @@
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 {            
-     noLoop();  
-}  
-public void draw()  
-{   
-	//not needed for this assignment
-}  
-public boolean isPrime(double dNum)  
-{   
-    //to be finished later   
-    return true;  
-} 
+	for(int i = 1; i<e.length()-10; i++){
+		String digits = e.substring(i,i+10);
+		double dNum = Double.parseDouble(digits);
+		if(isPrime(dNum)==true){
+			System.out.println(dNum);
+		}
+	}
+    noLoop();  
+
+}
+public boolean isPrime(double num){
+  if(num<=1){
+    return false;
+  }
+  for(int i = 2; i<=Math.sqrt(num); i++){
+    if(num%i==0){
+      return false;
+    }
+  }
+  
+  return true;
+}
---
+++ build-tmp/source/GoogleBillboard.java
@@ -0,0 +1,50 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class GoogleBillboard extends PApplet {
+
+public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+public void setup()  
+{            
+	for(int i = 1; i<e.length()-10; i++){
+		String digits = e.substring(i,i+10);
+		double dNum = Double.parseDouble(digits);
+		if(isPrime(dNum)==true){
+			System.out.println(dNum);
+		}
+	}
+    noLoop();  
+
+}
+public boolean isPrime(double num){
+  if(num<=1){
+    return false;
+  }
+  for(int i = 2; i<=Math.sqrt(num); i++){
+    if(num%i==0){
+      return false;
+    }
+  }
+  
+  return true;
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "GoogleBillboard" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
